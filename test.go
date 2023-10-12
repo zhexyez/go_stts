aa158912da34183d9274ace3b846759c447f6d47
Eugene <roreugene@gmail.com>
2023-10-12 18:14:03 +0300

    Factory is in place

--- test.go
+++ test.go
@@ -1,13 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // dummy type to make constants more understandable
 type TGate int
 
 // enumeration of constants to assign types to gates
 const (
-	IN TGate = iota
+	COMP TGate = iota
+	IN
 	OUT
 	NOT
 	AND
@@ -23,12 +26,14 @@ var wire_counter      int = 0
 type Pin struct {
 	number	int
 	p_state bool
+	w_connected bool
 	gate 	*Gate
 }
 
 // has type, state, array of pointers to pins that are connected
 // to it, a pointer to the wire to whuich it is connected
 type Gate struct {
+	name	string
 	number 	int
 	g_type	TGate
 	g_state bool
@@ -46,6 +51,7 @@ type Wire struct {
 type Component struct {
 	name		string
 	number      int
+	c_type		TGate
 	gates 		[]*Gate
 	pins  		[]*Pin
 	wires 		[]*Wire
@@ -53,9 +59,99 @@ type Component struct {
 	exposed_out *Wire
 }
 
-func NewComponent(name string, number_of_inputs int) Component {
+func NewComponent(name string, number_of_inputs int) *Component {
 	component_counter++
-	return Component{name: name, number: component_counter, exposed_in: make([]*Pin, number_of_inputs)}
+	return &Component{name: name, number: component_counter, c_type: COMP, exposed_in: make([]*Pin, number_of_inputs)}
+}
+
+func AddC2C(where_to_add *Component, what_to_add *Component) {
+	// implement
+}
+
+func NewPin(gate *Gate) *Pin {
+	pin_counter++
+	return &Pin{number: pin_counter, p_state: false, w_connected: false, gate: gate}
+}
+
+func NewGate(g_type TGate) *Gate {
+	gate_counter++
+	gate := Gate{g_type: g_type, number: gate_counter, g_state: false}
+	switch g_type {
+	case IN:
+		gate.name = "IN"
+		return &gate
+	case OUT:
+		gate.name = "OUT"
+		pin_in := NewPin(&gate)
+		gate.pins = append(gate.pins, pin_in)
+		return &gate
+	case NOT:
+		gate.name = "NOT"
+		pin_in := NewPin(&gate)
+		gate.pins = append(gate.pins, pin_in)
+		return &gate
+	case AND:
+		gate.name = "AND"
+		pin_in_1 := NewPin(&gate)
+		pin_in_2 := NewPin(&gate)
+		gate.pins = append(gate.pins, pin_in_1, pin_in_2)
+		return &gate
+	case OR:
+		gate.name = "OR"
+		pin_in_1 := NewPin(&gate)
+		pin_in_2 := NewPin(&gate)
+		gate.pins = append(gate.pins, pin_in_1, pin_in_2)
+		return &gate
+	default:
+		fmt.Println("Given type is unknown")
+		return nil
+	}
+}
+
+func NewWire(from_gate *Gate, to_pin *Pin) {
+	wire_counter++
+	wire := Wire{number: wire_counter, w_state: false, transmit: []*Pin{to_pin}}
+	from_gate.out_pin = &wire
+}
+
+func UpdateWire(wire *Wire, to_pin *Pin) {
+	wire.transmit = append(wire.transmit, to_pin)
+}
+
+func G2G(gate_from *Gate, gate_to *Gate, one_out_to_n_inputs int) {
+	
+	if (gate_from != nil) && (gate_to != nil) && (0 < one_out_to_n_inputs) && (one_out_to_n_inputs < 3){
+		
+		if gate_from.out_pin != nil {
+			
+			tmp_size := len(gate_from.out_pin.transmit)
+			for _, pin := range gate_to.pins {
+				if !pin.w_connected {
+					UpdateWire(gate_from.out_pin, pin)
+					pin.w_connected = true
+					break
+				}
+			}
+			if len(gate_from.out_pin.transmit) == tmp_size {
+				fmt.Println("Connection unsuccessful because there are no avialable pins on the Gate 2")
+			}
+
+		} else {
+			for _, pin := range gate_to.pins {
+				if !pin.w_connected {
+					NewWire(gate_from, pin)
+					pin.w_connected = true
+					break
+				}
+			}
+			if gate_from.out_pin == nil {
+				fmt.Println("Connection unsuccessful because there are no avialable pins on the Gate 2")
+			}
+		}
+
+	} else {
+		fmt.Println("Connection unsuccessful! RULES: Gate 1 cannot be nil | Gate 2 cannot be nil | Number of inputs should be either 1 or 2")
+	}
 }
 
 // called when wire should chage its state, the caller is gate
@@ -88,6 +184,40 @@ func (p *Pin) pin_report_state(rec_state bool) {
 // returns gate's state
 func (g *Gate) get_state() bool {
 	return g.g_state
+}
+
+func (g *Gate) get_detailed_state() {
+	fmt.Print("\n\n--- Selected gate detailed state ---\n\n")
+	fmt.Println("Number of gate element: ", g.number)
+	fmt.Println("Type name: ", g.name)
+	fmt.Println("State: ", g.g_state)
+	fmt.Println("Number of pins: ", len(g.pins))
+	for i, pin := range g.pins {
+		if pin == nil {
+			fmt.Println("_g_ The pin ", i, " is nil!")
+		} else {
+			fmt.Println("_g_ Number of pin element: ", pin.number)
+			fmt.Println("_g_ State: ", pin.p_state)
+			fmt.Println("_g_ Is connected: ", pin.w_connected)
+		}
+	}
+	if g.out_pin == nil {
+		fmt.Println("There is no output wire!")
+	} else {
+		fmt.Println("Number of wire element: ", g.out_pin.number)
+		fmt.Println("State: ", g.out_pin.w_state)
+		fmt.Println("Number of connections", len(g.out_pin.transmit))
+		for i, pin := range g.out_pin.transmit {
+			if pin == nil {
+				fmt.Println("_w_ The pin ", i, " is nil!")
+			} else {
+				fmt.Println("_w_ Number of pin element: ", pin.number)
+				fmt.Println("_w_ State: ", pin.p_state)
+				fmt.Println("_w_ Is connected: ", pin.w_connected)
+			}
+		}
+	}
+	fmt.Print("\n--- END ---\n\n")
 }
 
 // updates gate's state, calles update on wire if connected
@@ -130,88 +260,21 @@ func main() {
 	fmt.Print("\nSimple Truth Table Solver\n\n")
 
 	//var component = NewComponent("NAND Gate", 2)
+	var gate_and = NewGate(AND)
+	var gate_not = NewGate(NOT)
+	var gate_inbuff_1  = NewGate(IN)
+	var gate_inbuff_2  = NewGate(IN)
+	var gate_outbuff   = NewGate(OUT)
 
-	
+	G2G(gate_inbuff_1, gate_and, 1)
+	G2G(gate_inbuff_2, gate_and, 1)
+	G2G(gate_and, gate_not, 1)
+	G2G(gate_not, gate_outbuff, 1)
 
-	// declare all gates, i/o buffers are also gates
-	var gate_and Gate
-	var gate_not Gate
-	var gate_or  Gate
-	var in_buff1 Gate
-	var in_buff2 Gate
-	var out_buff Gate
+	gate_inbuff_1.update_state(true)
+	gate_inbuff_2.update_state(false)
 
-	// declare all pins
-	var pin_1	Pin
-	var pin_2	Pin
-	var pin_3	Pin
-	var pin_4	Pin
-	var pin_5	Pin
-	var pin_6	Pin
-
-	// declare all wires
-	var wire_1  Wire
-	var wire_2  Wire
-	var wire_3	Wire
-	var wire_4	Wire
-	var wire_5  Wire
-
-	// to use easily
-	var state_0 = false
-	var state_1 = true
-
-	// set types and states of gates, in gate has only output, out gate has only 1 input
-	in_buff1 = Gate{g_type: IN, g_state: state_0, out_pin: &wire_1}
-	in_buff2 = Gate{g_type: IN, g_state: state_0, out_pin: &wire_2}
-	out_buff = Gate{g_type: OUT, g_state: state_0, pins: []*Pin{&pin_6}}
-	gate_and = Gate{g_type: AND, g_state: state_0, pins: []*Pin{&pin_4, &pin_5}, out_pin: &wire_5}
-	gate_not = Gate{g_type: NOT, g_state: state_0, pins: []*Pin{&pin_3}, out_pin: &wire_3}
-	gate_or  = Gate{g_type: OR,  g_state: state_0, pins: []*Pin{&pin_1, &pin_2}, out_pin: &wire_4}
-
-	// set states of pins, connect to dedicated gates
-	pin_1 = Pin{p_state: state_0, gate: &gate_or}
-	pin_2 = Pin{p_state: state_0, gate: &gate_or}
-	pin_3 = Pin{p_state: state_0, gate: &gate_not}
-	pin_4 = Pin{p_state: state_0, gate: &gate_and}
-	pin_5 = Pin{p_state: state_0, gate: &gate_and}
-	pin_6 = Pin{p_state: state_0, gate: &out_buff}
-
-	// set states of wires, connect to dedicated pins
-	wire_1 = Wire{w_state: state_0, transmit: []*Pin{&pin_1}}
-	wire_2 = Wire{w_state: state_0, transmit: []*Pin{&pin_3, &pin_5}}
-	wire_3 = Wire{w_state: state_0, transmit: []*Pin{&pin_2}}
-	wire_4 = Wire{w_state: state_0, transmit: []*Pin{&pin_4}}
-	wire_5 = Wire{w_state: state_0, transmit: []*Pin{&pin_6}}
-
-	// can be used multiple times
-	in_buff1.update_state(state_0)
-	in_buff2.update_state(state_0)
-
-	// PRINTING
-	fmt.Print("\nInputs are: ", in_buff1.g_state, " and ", in_buff2.g_state, "\n")
-	fmt.Println("The output is: ", out_buff.get_state())
-
-	// can be used multiple times
-	in_buff1.update_state(state_0)
-	in_buff2.update_state(state_1)
-
-	// PRINTING
-	fmt.Print("\nInputs are: ", in_buff1.g_state, " and ", in_buff2.g_state, "\n")
-	fmt.Println("The output is: ", out_buff.get_state())
-
-	// can be used multiple times
-	in_buff1.update_state(state_1)
-	in_buff2.update_state(state_0)
-
-	// PRINTING
-	fmt.Print("\nInputs are: ", in_buff1.g_state, " and ", in_buff2.g_state, "\n")
-	fmt.Println("The output is: ", out_buff.get_state())
-
-	// can be used multiple times
-	in_buff1.update_state(state_1)
-	in_buff2.update_state(state_1)
-
-	// PRINTING
-	fmt.Print("\nInputs are: ", in_buff1.g_state, " and ", in_buff2.g_state, "\n")
-	fmt.Println("The output is: ", out_buff.get_state())
+	fmt.Print("\nInputs are: ", gate_inbuff_1.g_state, " and ", gate_inbuff_2.g_state, "\n")
+	//gate_and.get_detailed_state()
+	fmt.Println("The output is: ", gate_outbuff.get_state())
 }
